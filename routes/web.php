f792b3583de32574ee6b04dde4ed082e12719a4b
raman <ramanvellore2018@gmail.com>
2020-04-09 20:25:01 +0530

    Created Model -a

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::apiResource('/products','ProductController');
+
+Route::group(['prefix'=>'products'], function(){
+	Route::apiResource('/{product}/reviews','ReviewController');
+});
